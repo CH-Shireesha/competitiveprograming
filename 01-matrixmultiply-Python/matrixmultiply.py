ed29ef60d89377a39acca9202506a69e0dd12450
CH-Shireesha <shirishach26@msitprogram.net>
2020-07-14 11:03:34 +0530

    Jul 14  11:03:34

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -5,6 +5,12 @@
 
 
 def fun_matrixmultiply(m1, m2):
+    for i in range(len(m1)):
+        for j in range(len(m2[0])):
+            for k in range(len(m2)):
+                res[i][j] += m1[i][k] * m2[k][j]
+    for r in res:
+        return r
     return None
 
 
