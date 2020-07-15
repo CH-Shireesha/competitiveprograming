54ab32461c46f354e5e019e183672a69ff07279c
CH-Shireesha <shirishach26@msitprogram.net>
2020-07-15 20:16:00 +0530

    Jul 15  20:15:58

--- 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
+++ 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
@@ -10,13 +10,13 @@ def hasconsecutivedigits(n):
 	n //= 10
 	while(n > 0):
 		temp = n % 10
+		n //= 10
 		if (temp == prev):
 			return True
 			break
 		else:
 			prev = n%10
 			n //= 10
-		n //= 10
 	return False
 	
 	pass
