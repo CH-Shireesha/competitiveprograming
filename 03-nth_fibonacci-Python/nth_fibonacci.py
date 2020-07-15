fb1584504c506a2ec78c2c081b7dcf817b0f7163
CH-Shireesha <shirishach26@msitprogram.net>
2020-07-15 14:54:27 +0530

    Jul 15  14:54:27

--- 03-nth_fibonacci-Python/nth_fibonacci.py
+++ 03-nth_fibonacci-Python/nth_fibonacci.py
@@ -9,9 +9,10 @@
 
 def fun_nthfibonaccinumber(n):
 	# your code goes Here
-	if n == 0 and n == 1:
-		return 1
-	elif n == 2:
-		return 2
-	else:
-		return fun_nthfibonaccinumber(n-1) + fun_nthfibonaccinumber(n-2)
+	while n > 0:
+		if n == 0 and n == 1:
+			return 1
+		elif n == 2:
+			return 2
+		else:
+			return fun_nthfibonaccinumber(n-1) + fun_nthfibonaccinumber(n-2)
