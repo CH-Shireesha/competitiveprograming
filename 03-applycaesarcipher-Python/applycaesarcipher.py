416f55b35599680bffd748e7d1d9970704c4ad2c
CH-Shireesha <shirishach26@msitprogram.net>
2020-07-16 21:58:55 +0530

    Jul 16  21:58:54

--- 03-applycaesarcipher-Python/applycaesarcipher.py
+++ 03-applycaesarcipher-Python/applycaesarcipher.py
@@ -15,10 +15,10 @@ def fun_applycaesarcipher(msg, shift):
 	for i in range(len(msg)):
 		char = msg[i]
 		if (char.isupper()):
-			res += chr(ord(char) + shift + 65)
+			res += chr(ord(char) + shift)
 			print(ord(char) + shift)
 		else:
-			res += chr(ord(char) + shift + 97)
+			res += chr(ord(char) + shift)
 	return res
 
 fun_applycaesarcipher("We Attack At Dawn", 1)
