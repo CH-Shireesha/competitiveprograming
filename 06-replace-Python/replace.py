812d73ead6723d8d2cdc0e0841049953bc523230
CH-Shireesha <shirishach26@msitprogram.net>
2020-07-16 13:02:54 +0530

    Jul 16  13:02:53

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -10,7 +10,7 @@ def fun_replace(s1, s2, s3):
 	if s2 in s1:
 		substring = s1.find(s2)
 		n = substring + (len(s2))
-		prevsub = s1[:n]
+		prevsub = s1[:substring]
 		sub = s1[n:]
 		k = prevsub+s3+sub 
 	print(k)
