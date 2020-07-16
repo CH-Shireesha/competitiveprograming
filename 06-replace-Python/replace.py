916d50a22b5c9c89bb6f914446a432195f3e25f1
CH-Shireesha <shirishach26@msitprogram.net>
2020-07-16 13:03:28 +0530

    Jul 16  13:03:26

---
+++ 06-replace-Python/replace.py
@@ -0,0 +1,16 @@
+# Without using the builtin method s.replace(), 
+# write its equivalent. Specifically, write the function 
+# replace(s1, s2, s3) that returns a string equal to 
+# s1.replace(s2, s3), but again without calling s.replace().
+
+
+def fun_replace(s1, s2, s3):
+	res = []
+	k = ""
+	if s2 in s1:
+		substring = s1.find(s2)
+		n = substring + (len(s2))
+		prevsub = s1[:substring]
+		sub = s1[n:]
+		k = prevsub+s3+sub 
+	return k
