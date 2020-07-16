a3c2339a0b4f746536b27a315649c3da9a854d26
CH-Shireesha <shirishach26@msitprogram.net>
2020-07-16 14:30:33 +0530

    Jul 16  14:30:30

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -4,15 +4,20 @@
 # s1.replace(s2, s3), but again without calling s.replace().
 
 
+import re
+
 def fun_replace(s1, s2, s3):
 	res = []
 	k = ""
 	if s2 in s1:
-		substring = s1.find(s2)
-		n = substring + (len(s2))
-		prevsub = s1[:substring]
-		sub = s1[n:]
-		k = prevsub+s3+sub
+		for m in re.finditer(s2, s1):
+			substring = m.start()
+			n1 = m.end()
+			print(substring,n1)
+			# n = substring + n1
+			prevsub = s1[:substring]
+			# sub = s1[n:]
+			# k = prevsub+s3+sub
 		print(k) 
 		return k
 	else:
