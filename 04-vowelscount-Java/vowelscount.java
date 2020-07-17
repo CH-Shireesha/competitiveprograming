44eb2f41449f504908a519e734110e53ea2dfed1
CH-Shireesha <shirishach26@msitprogram.net>
2020-07-17 12:36:51 +0530

    Jul 17  12:36:49

--- 04-vowelscount-Java/vowelscount.java
+++ 04-vowelscount-Java/vowelscount.java
@@ -8,7 +8,7 @@ class vowelscount {
 	public int fun_vowelscount(String s){
 		// your code goes here
 		int count = 0;
-		for (int i = 0; i <= s.length(); i++){
+		for (int i = 0; i <= s.length()-1; i++){
 			if (s.charAt(i) == 'a' || s.charAt(i) == 'e' ||  s.charAt(i) == 'i' ||  s.charAt(i) == 'o' || 
 			 s.charAt(i) == 'u'|| s.charAt(i) == 'A' || s.charAt(i) == 'E' ||  s.charAt(i) == 'I' ||  s.charAt(i) == 'O' ||  s.charAt(i) == 'U') {
 				count = count + 1;
