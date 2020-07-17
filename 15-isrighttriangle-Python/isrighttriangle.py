04e0b5b6f501bcdd8a2b61feacd38f6d249784b2
CH-Shireesha <shirishach26@msitprogram.net>
2020-07-17 22:20:53 +0530

    Jul 17  22:20:51

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -7,4 +7,21 @@
 
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
+	a = distance(x1,y1,x2,y2)
+	b = distance(x2,y2,x3,y3)
+	c = distance(x3,y3,x1,y2)
+	m = a**2 + b**2
+	n = b**2 + c**2
+	l = c**2 + a**2
+	if (m == c**2):
+		return True
+	if(n == a**2):
+		return True
+	if(l == a**2):
+		return True
+	return False
 	pass
+
+def distance(x1,y1,x2,y2):
+	dist = floor(math.sqrt((x2 - x1)**2 + (y2 - y1)**2))
+	return dist
