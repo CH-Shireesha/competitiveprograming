1f13bae547eb53db248793f5d84be1b6421be231
CH-Shireesha <shirishach26@msitprogram.net>
2020-07-23 19:11:31 +0530

    Jul 23  19:11:31

--- 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
+++ 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
@@ -8,10 +8,10 @@
 def nthpowerfulnumber(n):
 	# Your code goes here
 	l = []
-	for i in range(n+1):
+	for i in range(10000):
 		if(ispowerful(i)):
 			l.append(i)
-	print(l)
+	print(l[n])
 
 
 def ispowerful(n):
@@ -26,7 +26,6 @@ def ispowerful(n):
 			n1 += 1
 		if(c == 1 or c == 2):
 			if(n%i == 0 and (n**2)%i == 0):
-				print(i)
 				return True
 	return False
 			
