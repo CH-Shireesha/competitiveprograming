0eb842434acaf56c05045796d730160a5c103094
CH-Shireesha <shirishach26@msitprogram.net>
2020-07-25 15:24:09 +0530

    Jul

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -7,7 +7,8 @@
 # so fun_nthsmithnumber(0) should return 4
 # so fun_nthsmithnumber(1) should return 22
 
-primes = []
+import math
+
 def fun_nth_smithnumber(n):
     j = 1
     while(j < n):
@@ -38,6 +39,24 @@ def isSmith(j):
         num //= 10
     return (sum == sumd)
 
+primes = []
+max = 1000
+def smallprime():
+    m = [0] * ((max//2)+100)
+    i = 1
+    while(i <= ((math.sqrt(max)-1)/2)):
+        j = (i* (i+1)) << 1
+        while(j <= max//2):
+            m[j] = 1
+            j += 2*i+1
+        i += 1
+    primes.append(2)
+    i = 1
+    while(i <= max/2):
+        if(m[i] == 0):
+            primes.append(2*i+1)
+        i += 1
 
+smallprime()
 print(isSmith(4))
 print(fun_nth_smithnumber(1))
