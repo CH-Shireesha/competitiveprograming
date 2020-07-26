5ea7ea7221e31829a6d5083ddd2beb0942e65b7d
CH-Shireesha <shirishach26@msitprogram.net>
2020-07-26 14:27:34 +0530

    Jul 26  14:27:34

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -10,6 +10,19 @@ class BST(object):
 
     def insert(self, new_val):
         # Your code goes here
+        if(self == None):
+            self = new_val
+        else:
+            if(self.value < node.value):
+                if(self.right is None):
+                    self.right = new_val
+                else:
+                    insert(self.right,new_val)
+            else:
+                if(self.left is None):
+                    self.left = new_val
+                else:
+                    insert(self.left,new_val)
         pass
 
     def printSelf(self):
