6757034d6622b52fe22bfadb0ebbd8279a8cb3fc
CH-Shireesha <shirishach26@msitprogram.net>
2020-07-27 01:03:01 +0530

    Jul 27  01:03:00

--- 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
+++ 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
@@ -11,4 +11,41 @@
 import math
 
 def fun_nth_lefttruncatableprime(n):
-    return 1
+    num = 1
+    c = 0
+    while(c <= n):
+        if(isprime(num)):
+            l = []
+            temp = num
+            while(temp > 0):
+                rem = temp%10
+                l.append(rem)
+                temp //= 10
+            l = l[::-1]
+            if i not in l:
+                result = i
+                x = i
+                for j in range(len(l)):
+                    result = result + l[j]*(10**(len(l)-j-1))
+                    if(isprime(num-result)):
+                        x += 1
+                    else:
+                        break
+                if(x == len(l)-1):
+                    c += 1
+        num += 1
+
+    return (num-1)
+
+def isprime(n):
+    i = 1
+    c = 0
+    while(i <= n):
+        if(n%i == 0):
+            c += 1
+            if(c > 2):
+                break
+        i += 1
+    if(c == 2):
+        return True
+    return False
