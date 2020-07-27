420324c3126eaaf042da36554c3d19a27cd4e6b2
CH-Shireesha <shirishach26@msitprogram.net>
2020-07-27 12:05:26 +0530

    Jul 27  12:05:26

--- 09-shortenlongruns-Python/shortenlongruns.py
+++ 09-shortenlongruns-Python/shortenlongruns.py
@@ -11,11 +11,11 @@
 
 def shortenlongruns(L, k):
 	# Your code goes here
-	num = 0
 	lst = []
 	for i in range(len(L)):
 		if L[i] not in lst:
 			lst.append(L[i])
+			num = 1
 		elif(L[i] != L[i-1]):
 			lst.append(L[i])
 		elif(L[i] == L[i-1]):
